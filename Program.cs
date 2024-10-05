c1ddc53d7067f13474c0b1f758b67698e544b4e8
dvinh3406 <pdv206@gmail.com>
2024-10-05 16:16:56 +0700

    add
    add

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("nhap diem trung binh cua sinh vien");
+        float score = float.Parse(Console.ReadLine());
+        if ( 0 <=score&&score<5) Console.WriteLine("yeu");
+        else if ( 5 <=score&&score<7) Console.WriteLine("trung binh");
+        else if ( 7 <=score&&score<8) Console.WriteLine("kha");
+        else if ( 8 <=score&&score<10) Console.WriteLine("gioi");
+        else Console.WriteLine("nhap sai gia tri diem so");
+    }
+}
